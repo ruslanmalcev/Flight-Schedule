eaff14adc99e6a57167063f98c23c55e4ee5375d
ruslanmalcev <ruslanmalcev.83@gmail.com>
2026-01-22 22:06:39 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,57 +3,48 @@
 #include <conio.h>
 #include "functions.h"
 
-#define P printf
-#define S scanf
-#define N 45
-#define M 15
 
 int main()
 {
     system("color 70");
-    int n; // number of elements
-    Flight* pt;
-    n=35;
-    pt = (Flight*)malloc(n * sizeof(Flight));
-    initialize(pt,n); // initializes the array
+    int flights_number = 99;
+    Flight* flights = (Flight*)malloc(flights_number * sizeof(Flight));
+    initialize(flights, flights_number); // initializes the array
 
 //----------MAIN_MENU----------------//
     while(1)
     {
       system("cls");
-        P("\n\t\t\t CHOOSE ITEM OF MENU.......:\n");
-        P("\n\t\t\t PRINT TABLE...............0");
-        P("\n\t\t\t INPUT DATA................1");
-        P("\n\t\t\t OUTPUT DATA...............2");
-        P("\n\t\t\t SEARCH MIN VALUE..........3");
-        P("\n\t\t\t SEARCH FOR VALUE..........4");
-        P("\n\t\t\t SORTING...................5");
-        P("\n\t\t\t DELETE ELEMENT............6");
-        P("\n\t\t\t EDIT ELEMENT..............7");
-        P("\n\t\t\t CALCULATE TICKET PRICE....8");
-        P("\n\t\t\t CLEAR LINE................9\n");
-        P("\n\t\t\t EXIT......................Esc");
+        printf("\n\t\t\t CHOOSE ITEM OF MENU.......:\n");
+        printf("\n\t\t\t PRINT TABLE...............0");
+        printf("\n\t\t\t INPUT DATA................1");
+        printf("\n\t\t\t OUTPUT DATA...............2");
+        printf("\n\t\t\t SEARCH MINIMAL............3");
+        printf("\n\t\t\t SEARCH FOR VALUE..........4");
+        printf("\n\t\t\t SORTING...................5");
+        printf("\n\t\t\t EDIT ELEMENT..............6");
+        printf("\n\t\t\t CALCULATE TICKET PRICE....7");
+        printf("\n\t\t\t CLEAR LINE................8\n");
+        printf("\n\t\t\t EXIT......................Esc");
       switch (getch())
        {            // call function
-         case '9': clear_entry(pt,n);
+         case '0': output_table(flights, flights_number);
             break;
-         case '0': output_table(pt,n);
+         case '1': input_data(flights,flights_number);
             break;
-         case '1': input_data(pt,n);
-            break;
-         case '2': output_menu(pt,n);
+         case '2': output_menu(flights, flights_number);
           break;
-         case '3': search_min_menu(pt,n);
+         case '3': search_min_menu(flights, flights_number);
             break;
-         case '4': search_menu_for_value(pt,n);
+         case '4': search_menu_for_value(flights, flights_number);
             break;
-         case '5': sort_menu_for_value(pt,n);
+         case '5': sort_menu_for_value(flights, flights_number);
             break;
-         case '6': delete_element(pt,&n);
+         case '6': edit(flights, flights_number);
             break;
-         case '7': edit(pt,n);
+         case '7': average_price(flights, flights_number);
             break;
-         case '8': average_price(pt,n);
+         case '8': clear_entry(flights, flights_number);
             break;
          case 27:  // exit attempt
              {
@@ -62,8 +53,8 @@ int main()
                 case 1: // exit
                     {
                         int count = 0;
-                        for(; count < n; ++count){
-                            free(pt + count);
+                        for(; count < flights_number; ++count){
+                            free(flights + count);
                         }
                         return 0;
                     }
